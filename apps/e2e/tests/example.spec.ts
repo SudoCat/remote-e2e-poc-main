6f2f46a9030e5eb9fd8b517218122f7512d3dcd8
Robin Neal <howdy@robinneal.co.uk>
2023-04-14 13:23:18 +0100

    fix e2e test runner

--- apps/e2e/tests/example.spec.ts
+++ apps/e2e/tests/example.spec.ts
@@ -2,15 +2,15 @@ import { test, expect } from '@playwright/test';
 
 const url = process.env.TEST_ENV_DOMAIN || 'http://localhost:3060'
 
-test('main route has title', async ({ page }) => {
+test('main route has text', async ({ page }) => {
   await page.goto(`${url}/`);
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/web/i);
+  await expect(page.getByText(/web/i)).toBeVisible()
 });
 
-test('get started link', async ({ page }) => {
+test('sub route has text', async ({ page }) => {
   await page.goto(`${url}/sub`);
 
-  await expect(page).toHaveTitle(/Express/i);
+  await expect(page.getByRole('heading', { name: /Welcome to Astro/i })).toBeVisible()
 });
